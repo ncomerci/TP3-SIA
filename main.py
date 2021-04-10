95ed9b86d00eea2babf944c4903377fe709cad56
Nicolás Comerci <ncomerci@itba.edu.ar>
2021-04-09 21:17:55 -0300

    config file created

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 from perceptrons.simple_perceptron import SimplePerceptron
 from perceptrons.linear_simple_perceptron import LinearSimplePerceptron
 from perceptrons.non_linear_simple_perceptron import NonLinearSimplePerceptron
-import csv
+import csv, json
 
-training = [[-1, 1], [1, -1], [-1, -1], [1, 1]]
-and_output = [-1, -1, -1, 1]
-xor_output = [1, 1, -1, -1]
-or_output = [1, 1, -1, 1]
+with open("config.json") as f:
+    config = json.load(f)
 
-training_file = open("training_set2.tsv")
-output_file = open("expected_output2.tsv")
-read_tsv = csv.reader(training_file, delimiter="\t")
-read_tsv2 = csv.reader(output_file, delimiter="\t")
+training_file = open(config["training_tsv_path"])
+output_file = open(config["output_tsv_path"])
+learning_rate = config["learning_rate"]
+perceptrons = {
+    "step_simple_perceptron": SimplePerceptron,
+    "linear_simple_perceptron": LinearSimplePerceptron,
+    "non_linear_simple_perceptron": NonLinearSimplePerceptron
+}
+perceptron = config["perceptron_type"]
+max_iterations = config["max_iterations"]
+
+
+read_training_tsv = csv.reader(training_file, delimiter="\t")
+read_output_tsv = csv.reader(output_file, delimiter="\t")
 
 training_set = []
-for row in read_tsv:
+for row in read_training_tsv:
     training_set.append(list(map(lambda x: float(x), row)))
-expected = []
-for row in read_tsv2:
-    expected.append(list(map(lambda x: float(x), row)))
-#sp = LinearSimplePerceptron(training_set, expected, 0.01)
-sp = NonLinearSimplePerceptron(training_set, expected, 0.01)
+    
+expected_output = []
+for row in read_output_tsv:
+    expected_output.append(list(map(lambda x: float(x), row)))
 
-sp.train(10000)
+sp = perceptrons[perceptron](training_set, expected_output, learning_rate)
 
-print("ACTIVATION:", sp.get_output([[4.4793,-4.0765,4.4558]]))
+sp.train(max_iterations)
+
+print("ACTIVATION:", sp.get_output(training_set))
