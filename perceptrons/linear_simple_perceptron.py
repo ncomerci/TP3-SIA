0b2c1b14e837145fe5803e96152e56db11a4e1c5
Nicolás Comerci <ncomerci@itba.edu.ar>
2021-04-09 22:06:32 -0300

    error calculation changed

--- perceptrons/linear_simple_perceptron.py
+++ perceptrons/linear_simple_perceptron.py
@@ -13,7 +13,9 @@ class LinearSimplePerceptron(Perceptron):
         error = 0
         for i in range(len(self.training_set)):
             activation_state = np.inner(self.training_set[i], w) # internal product: sum (e[i_x]*w_i) --> hiperplano
-            error += (self.expected_output[i] - activation_state)**2
-        return 0.5 * error
+            # error += (self.expected_output[i] - activation_state)**2
+            error += abs(self.expected_output[i] - activation_state)
+        # return 0.5 * error
+        return error
 
         
--- perceptrons/non_linear_simple_perceptron.py
+++ perceptrons/non_linear_simple_perceptron.py
@@ -8,12 +8,14 @@ class NonLinearSimplePerceptron(Perceptron):
         super().__init__(training_set,expected_output,learning_rate)
     
     def activation(self, excited_state):
-        return math.tanh(excited_state)
+        return np.tanh(excited_state)
          
     def error(self,w):
         error = 0
         for i in range(len(self.training_set)):
             excited_state = np.inner(self.training_set[i], w) # internal product: sum (e[i_x]*w_i) --> hiperplano
             activation_state = self.activation(excited_state)
-            error += (self.expected_output[i] - activation_state)**2
-        return 0.5 * error
+            # error += (self.expected_output[i] - activation_state)**2
+            error += abs(self.expected_output[i] - activation_state)
+        # return 0.5 * error
+        return error
